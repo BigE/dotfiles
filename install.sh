0563adddf08cfbc14f338dc85742654455c92f7a
Eric Gach <eric.gach@gmail.com>
2018-01-04 16:00:08 -0600

    updating for various changes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "$PWD" != "$DIR" ]]
@@ -7,19 +7,8 @@ then
     exit 1;
 fi
 
-if [[ ! -d $PWD/zsh/oh-my-zsh ]]; then
-    echo "ERROR: You must run \`git submodule update --init --recursive\` first"
-fi
-
-if [ -d $HOME/.byobu ] && [ ! -L $HOME/.byobu ]; then
-    rm -Rf $HOME/.byobu.bak
-    mv -f $HOME/.byobu $HOME/.byobu.bak
-fi
-
-if [ -d $HOME/.oh-my-zsh ]; then
-    rm -Rf $HOME/.oh-my-zsh.bak
-    mv -f $HOME/.oh-my-zsh $HOME/.oh-my-zsh.bak
-fi
+# ensure we're up to date
+git submodule update --init
 
 if [ -d $HOME/.vim ]; then
     rm -Rf $HOME/.vim.bak
@@ -27,16 +16,10 @@ if [ -d $HOME/.vim ]; then
 fi
 
 ln -sf $DIR/bashrc $HOME/.bashrc
-if [ ! -L $HOME/.byobu ]; then
-    ln -sf $DIR/byobu $HOME/.byobu
-fi
 ln -sf $DIR/commonrc $HOME/.commonrc
 ln -sf $DIR/gitconfig $HOME/.gitconfig
 ln -sf $DIR/gitignore $HOME/.gitignore
 ln -sf $DIR/gvimrc $HOME/.gvimrc
-if [ ! -L $HOME/.oh-my-zsh ]; then
-    ln -sf $DIR/zsh/oh-my-zsh $HOME/.oh-my-zsh
-fi
 if [ ! -L $HOME/.vim ]; then
     ln -sf $DIR/vim $HOME/.vim
 fi
