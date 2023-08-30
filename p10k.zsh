7108796d0223007c0e030e2731f516c91681c22b
Eric Gach <eric.gach@gmail.com>
2023-08-30 11:21:28 -0500

    Adding ssh icon

--- p10k.zsh
+++ p10k.zsh
@@ -33,7 +33,8 @@
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
-    context                 # user@hostname
+    SSH                     # ssh session
+    context_joined          # user@hostname
     dir                     # current directory
     vcs                     # git status
     # =========================[ Line #2 ]=========================
